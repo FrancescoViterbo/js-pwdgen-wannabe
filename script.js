9cbf5f5920e1e6966c25426052f2d9836c8c0dc0
FrancescoViterbo <franc.viterbo@gmail.com>
2020-10-19 13:28:53 +0200

    esercizio completato

---
+++ script.js
@@ -0,0 +1,5 @@
+var nome = prompt("Scrivi il tuo nome");
+var cognome = prompt("Scrivi il tuo cognome");
+var colore = prompt("Scrivi il tuo colore preferito");
+
+window.alert("La tua password è: " + nome + cognome + colore + "19");
